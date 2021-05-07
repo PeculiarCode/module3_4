21b5f456f1e49fd14fce4db4949571bf24c85dfc
PeculiarCode <tunshishky@outlook.com>
2021-05-08 00:19:52 +0800

    blog

--- src/api/index.js
+++ src/api/index.js
@@ -12,16 +12,26 @@ export const getFollowerList = (pageIndex, pageSize) => {
 export const getFollowingList = (pageIndex, pageSize) => {
   return request({
     url: '/users/GitHub-Laziji/following',
-    method:'GET',
+    method: 'GET',
     params: {
       page: pageIndex,
       per_page: pageSize
     }
   })
 }
-export const getUserDetail = (user) => {
+export const getUserDetail = user => {
   return request({
     url: `/users/${user}`,
-    method:'GET'
+    method: 'GET'
+  })
+}
+export const getBlog = (pageIndex, pageSize) => {
+  return request({
+    url: `/users/GitHub-Laziji/gists`,
+    method: 'GET',
+    params: {
+      page: pageIndex,
+      per_page: pageSize
+    }
   })
 }
